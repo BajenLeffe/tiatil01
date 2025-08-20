cab36187525914dc367d62b82836257d3b3ff085
sonicExe124 <alexandros.muller07@gmail.com>
2025-08-20 10:07:06 +0200

    första uppgift uppe

---
+++ monthlypayment.py
@@ -0,0 +1,20 @@
+def calculate_monthly_payment(principal, annual_rate, years):
+    r = (annual_rate / 100) / 12
+
+    n = years * 12
+
+    if r == 0:
+        return principal / n 
+
+    A = (principal * r) / (1 - (1 + r) ** -n)
+    return A
+
+
+principal = float(input("Lånebelopp: "))
+
+annual_rate = float(input("Årsränta (%): "))
+
+years = int(input("Löptid (år): "))
+
+payment = calculate_monthly_payment(principal, annual_rate, years)
+print(f"Din månatliga betalning är: {payment:.2f} kr")
